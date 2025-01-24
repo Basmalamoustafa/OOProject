26509994aa89481a9a65eee02f398d2c8bbda17e
Basmalamoustafa <75669135+Basmalamoustafa@users.noreply.github.com>
2025-01-24 13:32:00 +0200

    Add files via upload

---
+++ Management System/Promotion.h
@@ -0,0 +1,13 @@
+#pragma once
+class Promotion
+{
+private:
+	double promotionPercentage, price, priceAfterPromotion;
+	string code;
+public:
+	Promotion();
+	double PriceAfterPromotion(double promotionPercentage, double price);
+	void setPercentage(double promotionPercentage);
+	double getPercentage();
+	void Code(string code);
+};
